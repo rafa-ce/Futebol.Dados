90f4f2ee2bf5a5c89150359846ab1e49436ab3ca
Rafa C <rafasantcont@yahoo.com.br>
2019-08-07 00:09:22 -0300

    Registrar as configurações do mongodb no contêiner de DI

--- Futebol.Dados.Api/Startup.cs
+++ Futebol.Dados.Api/Startup.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Futebol.Dados.Api.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace Futebol.Dados.Api
 {
@@ -13,10 +17,23 @@ namespace Futebol.Dados.Api
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
 
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<FutebolDatabaseSettings>(
+                Configuration.GetSection(nameof(FutebolDatabaseSettings)));
+
+            services.AddSingleton<IFutebolDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<FutebolDatabaseSettings>>().Value);
+
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
@@ -24,6 +41,8 @@ namespace Futebol.Dados.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMvc();
 
             app.Run(async (context) =>
             {
